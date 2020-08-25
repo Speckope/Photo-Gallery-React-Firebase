ad64bad1069dfaa57aa7d56acfb1bc84a9676500
Speckope <Speckope@gmail.com>
2020-08-25 18:06:41 +0200

    added styles and input component with file.type validation

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { projectStorage } from '../firebase/config';
 
 const useStorage = (file) => {
-    const [progress, setProgress] = useState(0)
-    const [error, setError] = useState(null)
-    const [url, setUrl] = useState(null)
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
 
-    return (  );
-}
- 
-export default useStorage ;
+  return <div></div>;
+};
+
+export default useStorage;
